59e7a1093f7c8a8a4849ca3d3b4c69b3b4bbe63e
taojigu <taojigu@163.com>
2015-09-09 17:37:57 +0800

    add project

---
+++ Demo/Classes/SMTPSenderAppDelegate.m
@@ -0,0 +1,147 @@
+//
+//  SMTPSenderAppDelegate.m
+//  SMTPSender
+//
+//  Created by Ian Baird on 10/28/2008.
+//
+//  Copyright (c) 2008 Skorpiostech, Inc. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "SMTPSenderAppDelegate.h"
+#import "SKPSMTPMessage.h"
+#import "NSData+Base64Additions.h"
+
+@implementation SMTPSenderAppDelegate
+
+
+
++ (void)initialize {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *defaultsDictionary = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"monica@friends.com", @"fromEmail",
+                                               @"rachel@friends.com", @"toEmail",
+                                               @"smtp.qiye.163.com", @"relayHost",
+                                               @"monica@friends.com", @"login",
+                                               @"123456", @"pass",
+                                               [NSNumber numberWithBool:YES], @"requiresAuth",
+                                               [NSNumber numberWithBool:YES], @"wantsSecure", nil];
+    
+    [userDefaults registerDefaults:defaultsDictionary];
+}
+- (void)applicationDidFinishLaunching:(UIApplication *)application {    
+    
+    // Override point for customization after app launch    
+    [self.window makeKeyAndVisible];
+}
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self updateTextView];
+}
+
+
+
+- (void)updateTextView {
+    NSMutableString *logText = [[NSMutableString alloc] init];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    [logText appendString:@"Use the iOS Settings app to change the values below.\n\n"];
+    [logText appendFormat:@"From: %@\n", [defaults objectForKey:@"fromEmail"]];
+    [logText appendFormat:@"To: %@\n", [defaults objectForKey:@"toEmail"]];
+    [logText appendFormat:@"Host: %@\n", [defaults objectForKey:@"relayHost"]];
+    [logText appendFormat:@"Auth: %@\n", ([[defaults objectForKey:@"requiresAuth"] boolValue] ? @"On" : @"Off")];
+    
+    if ([[defaults objectForKey:@"requiresAuth"] boolValue]) {
+        [logText appendFormat:@"Login: %@\n", [defaults objectForKey:@"login"]];
+        [logText appendFormat:@"Password: %@\n", [defaults objectForKey:@"pass"]];
+    }
+    [logText appendFormat:@"Secure: %@\n", [[defaults objectForKey:@"wantsSecure"] boolValue] ? @"Yes" : @"No"];
+    self.textView.text = logText;
+
+
+}
+
+- (IBAction)sendMessage:(id)sender {
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    SKPSMTPMessage *testMsg = [[SKPSMTPMessage alloc] init];
+    testMsg.fromEmail = [defaults objectForKey:@"fromEmail"];
+    
+    testMsg.toEmail = [defaults objectForKey:@"toEmail"];
+    testMsg.bccEmail = [defaults objectForKey:@"bccEmal"];
+    testMsg.relayHost = [defaults objectForKey:@"relayHost"];
+    
+    testMsg.requiresAuth = [[defaults objectForKey:@"requiresAuth"] boolValue];
+    
+    if (testMsg.requiresAuth) {
+        testMsg.login = [defaults objectForKey:@"login"];
+        
+        testMsg.pass = [defaults objectForKey:@"pass"];
+
+    }
+    
+    testMsg.wantsSecure = [[defaults objectForKey:@"wantsSecure"] boolValue]; // smtp.gmail.com doesn't work without TLS!
+
+    
+    testMsg.subject = @"SMTPMessage Test Message";
+    //testMsg.bccEmail = @"testbcc@test.com";
+    
+    // Only do this for self-signed certs!
+    // testMsg.validateSSLChain = NO;
+    testMsg.delegate = self;
+    
+    NSDictionary *plainPart = [NSDictionary dictionaryWithObjectsAndKeys:@"text/plain; charset=UTF-8",kSKPSMTPPartContentTypeKey,
+                               @"This is a tést messåge.",kSKPSMTPPartMessageKey,@"8bit",kSKPSMTPPartContentTransferEncodingKey,nil];
+    
+    NSString *vcfPath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"vcf"];
+    NSData *vcfData = [NSData dataWithContentsOfFile:vcfPath];
+    
+    NSDictionary *vcfPart = [NSDictionary dictionaryWithObjectsAndKeys:@"text/directory;\r\n\tx-unix-mode=0644;\r\n\tname=\"test.vcf\"",kSKPSMTPPartContentTypeKey,
+                             @"attachment;\r\n\tfilename=\"test.vcf\"",kSKPSMTPPartContentDispositionKey,[vcfData encodeBase64ForData],kSKPSMTPPartMessageKey,@"base64",kSKPSMTPPartContentTransferEncodingKey,nil];
+    
+    testMsg.parts = [NSArray arrayWithObjects:plainPart,vcfPart,nil];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [testMsg send];
+    });
+}
+- (void)messageSent:(SKPSMTPMessage *)message
+{
+
+    
+    self.textView.text  = @"Yay! Message was sent!";
+    //NSLog(@"delegate - message sent");
+}
+
+- (void)messageFailed:(SKPSMTPMessage *)message error:(NSError *)error
+{
+    
+    //self.textView.text = [NSString stringWithFormat:@"Darn! Error: %@, %@", [error code], [error localizedDescription]];\
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+            self.textView.text = [NSString stringWithFormat:@"Darn! Error!\n%li: %@\n%@", [error code], [error localizedDescription], [error localizedRecoverySuggestion]];
+    });
+    
+
+}
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  SMTPSender
+//
+//  Created by Ian Baird on 10/27/08.
+//  Copyright Skorpiostech, Inc 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SMTPSenderAppDelegate.h"
+
+int main(int argc, char *argv[]) {
+    
+
+    @autoreleasepool
+    {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMTPSenderAppDelegate class]));
+    }
+
+}
